7510211ad5ba8619e6c0cc1bdfcecb949e0e7530
Piotr Migdal <pmigdal@gmail.com>
2014-08-13 18:26:15 +0200

    x y to i j

--- main.js
+++ main.js
@@ -17,11 +17,11 @@ var TIME_STEP = 1000;
 var board = [];
 var state = {}; // or some hashtable like-tking
 // as of now
-// {"9,12,2": {x: 9, y: 12, dir: 2, amp: 1.}}
+// {"9,12,2": {i: 9, j: 12, dir: 2, amp: 1.}}
 // as of now, no polarizarion and only real amp
 var history2 = [];
 
-var tiles2colors = {empty: null,
+var tiles2colors = {emptj: null,
                     super_mirror: "black",
                     beam_splitter_a: "blue"};
 
@@ -46,11 +46,11 @@ var main = function () {
   board[4][4] = "beam_splitter_a";
   board[4][5] = "super_mirror";
 
-  v = {x: 6, y: 4, dir: 2, amp: 0.7};
-  state[[v.x, v.y, v.dir]] = v;
+  v = {i: 6, j: 4, dir: 2, amp: 0.7};
+  state[[v.i, v.j, v.dir]] = v;
 
-  v = {x: 4, y: 2, dir: 3, amp: 0.5};
-  state[[v.x, v.y, v.dir]] = v;
+  v = {i: 4, j: 2, dir: 3, amp: 0.5};
+  state[[v.i, v.j, v.dir]] = v;
 
   history2 = [];
 
@@ -60,7 +60,7 @@ var main = function () {
     history2.push([]);
     for (k in state) {
       v = state[k];
-      history2[i].push({x: v.x, y: v.y, dir: v.dir, amp: v.amp});  // to copy
+      history2[i].push({i: v.i, j: v.j, dir: v.dir, amp: v.amp});  // to copy
     }
 
     state = propagate(state, board);
@@ -77,13 +77,13 @@ function visualize () {
   var boardFlat = [];
 
   var drag = d3.behavior.drag()
-    .origin(function(d) { return {x: TILE_SIZE/2 + TILE_SIZE * d.x, y: TILE_SIZE/2 + TILE_SIZE * d.y}; })
+    .origin(function(d) { return {i: TILE_SIZE/2 + TILE_SIZE * d.i, j: TILE_SIZE/2 + TILE_SIZE * d.j}; })
     .on("drag", dragmove)
     .on("dragend", function () { console.log("Dragged!"); });
 
   for (i = 0; i < SIZE_X; i++) {
     for (j = 0; j < SIZE_Y; j++)
-      boardFlat.push({x: i, y: j, val: board[i][j]});
+      boardFlat.push({i: i, j: j, val: board[i][j]});
   }
 
   d3.select("svg").append("g")
@@ -95,8 +95,8 @@ function visualize () {
         .attr("class", "tile")
         .attr("width", 0.95 * TILE_SIZE)
         .attr("height", 0.95 * TILE_SIZE)
-        .attr("x", function (d) { return TILE_SIZE/2 + TILE_SIZE * d.x; })
-        .attr("y", function (d) { return TILE_SIZE/2 + TILE_SIZE * d.y; })
+        .attr("x", function (d) { return TILE_SIZE/2 + TILE_SIZE * d.i; })
+        .attr("y", function (d) { return TILE_SIZE/2 + TILE_SIZE * d.j; })
         .style("fill", function (d) { return tiles2colors[d.val]; })
         .call(drag);
 
@@ -104,8 +104,8 @@ function visualize () {
 
   function dragmove(d) {
     d3.select(this)
-      .attr("x", d3.event.x)
-      .attr("y", d3.event.y);
+      .attr("x", d3.event.i)
+      .attr("y", d3.event.j);
   }
 
 
@@ -125,16 +125,16 @@ function vizStep (i) {
     .append("circle")
       .attr("class", "photon")
       .attr("r", 10)
-      .attr("cx", function (d) { return TILE_SIZE + TILE_SIZE * (d.x - dir2vx(d.dir)); })  // as d.x and d.y are for destination location
-      .attr("cy", function (d) { return TILE_SIZE + TILE_SIZE * (d.y - dir2vy(d.dir)); })
+      .attr("cx", function (d) { return TILE_SIZE + TILE_SIZE * (d.i - dir2vx(d.dir)); })  // as d.i and d.j are for destination location
+      .attr("cy", function (d) { return TILE_SIZE + TILE_SIZE * (d.j - dir2vy(d.dir)); })
       .style("opacity", function (d) { return Math.abs(d.amp); })
       .style("fill", function (d) { return d.amp < 0 ? "violet" : null; });
 
   photons.transition()
     .ease([0,1])
     .duration(TIME_STEP)
-      .attr("cx", function (d) { return TILE_SIZE + TILE_SIZE * d.x; })
-      .attr("cy", function (d) { return TILE_SIZE + TILE_SIZE * d.y; });
+      .attr("cx", function (d) { return TILE_SIZE + TILE_SIZE * d.i; })
+      .attr("cy", function (d) { return TILE_SIZE + TILE_SIZE * d.j; });
 
   if ( i + 1 < history2.length ) {
     setTimeout(vizStep, TIME_STEP, i + 1);
@@ -152,40 +152,40 @@ function propagate (state0, board) {
   for (k in state0) {
     v0 = state0[k];
 
-    if ( (v0.x < 0) || (v0.x >= SIZE_X) || (v0.y < 0) || (v0.y >= SIZE_Y) ) {
+    if ( (v0.i < 0) || (v0.i >= SIZE_X) || (v0.j < 0) || (v0.j >= SIZE_Y) ) {
       console.log(v0);
       continue;
     }
 
-    tile = board[v0.x][v0.y];
+    tile = board[v0.i][v0.j];
 
     // v1 - warning: in most cases there are plenty outputs
 
     switch ( tile ) {
       case "empty":
         // deserves a separate function as it is used in all other
-        v1s = [{x: v0.x, y: v0.y, dir: v0.dir, amp: v0.amp}];
-        v1s[0].x = v0.x + dir2vx(v0.dir);
-        v1s[0].y = v0.y + dir2vy(v0.dir);
+        v1s = [{i: v0.i, j: v0.j, dir: v0.dir, amp: v0.amp}];
+        v1s[0].i = v0.i + dir2vx(v0.dir);
+        v1s[0].j = v0.j + dir2vy(v0.dir);
         break;
       case "super_mirror":  // simplest
-        v1s = [{x: v0.x, y: v0.y, dir: v0.dir, amp: v0.amp}];
-        v1s[0].x = v0.x - dir2vx(v0.dir);
-        v1s[0].y = v0.y - dir2vy(v0.dir);
+        v1s = [{i: v0.i, j: v0.j, dir: v0.dir, amp: v0.amp}];
+        v1s[0].i = v0.i - dir2vx(v0.dir);
+        v1s[0].j = v0.j - dir2vy(v0.dir);
         v1s[0].dir = (v0.dir + 2) % 4;
         break;
       case "beam_splitter_a":
         // antidiagonal; hadamard; not to careful with the sing
         v1s = [];
         // going forward
-        v1s.push({x:   v0.x + dir2vx(v0.dir),
-                  y:   v0.y + dir2vy(v0.dir),
+        v1s.push({i:   v0.i + dir2vx(v0.dir),
+                  j:   v0.j + dir2vy(v0.dir),
                   dir: v0.dir,
                   amp: v0.amp/Math.sqrt(2)})
         // bouncing
         var dir1 = v0.dir ^ 1;  // {0:1, 1:0, 2:3, 3:2} 
-        v1s.push({x:   v0.x + dir2vx(dir1),
-                  y:   v0.y + dir2vy(dir1),
+        v1s.push({i:   v0.i + dir2vx(dir1),
+                  j:   v0.j + dir2vy(dir1),
                   dir: dir1,
                   amp: (2 * (dir1 % 2) - 1) * v0.amp/Math.sqrt(2)})  // sign swap for one possibility
         break;
@@ -195,7 +195,7 @@ function propagate (state0, board) {
 
       v1 = v1s[i];
 
-      h = [v1.x, v1.y, v1.dir].join(",");  // we can do numerical, if needed for a speedup
+      h = [v1.i, v1.j, v1.dir].join(",");  // we can do numerical, if needed for a speedup
 
       if (h in state1) {
         state1[h].amp += v1.amp;
