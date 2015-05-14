9a6766de13934bd9bb4d1e9c4bcdde178bc66e4e
Piotr Migdal <pmigdal@gmail.com>
2015-05-14 12:06:27 +0200

    board draw init

--- board.js
+++ board.js
@@ -1,6 +1,14 @@
 // require mechanics.js
 // require elements.js
 
+var TILE_SIZE = 20;
+
+var i2x = function (i) { return TILE_SIZE/2 + TILE_SIZE * i; }
+var j2y = function (j) { return TILE_SIZE/2 + TILE_SIZE * j; }
+
+var x2i = function (x) { return Math.floor(x / TILE_SIZE); }
+var y2j = function (y) { return Math.floor(y / TILE_SIZE); }
+
 function Board (nX, nY) {
 
   this.nX = nX;
@@ -20,10 +28,45 @@ function Board (nX, nY) {
 
   this.stateSpatial = [];
 
+  for (i = 0; i < nX; i++) {
+    this.stateSpatial[i] = [];
+    for (j = 0; j < nY; j++) {
+      this.stateSpatial[i][j] = {};
+    }
+  }
+
+}
+
+Board.prototype.draw = function () {
+
+  var i, j, v, s;
+  var boardFlat = [];
+  
+  for (i = 0; i < this.nX; i++) {
+    for (j = 0; j < this.nY; j++) {
+      v = this.board[i][j];
+      boardFlat.push({i: i, j: j, val: v});
+    }
+  }
+
+  d3.select("svg").append("g")
+    .attr("id", "board")
+    .selectAll(".tile")
+    .data(boardFlat)
+    .enter()
+      .append("rect")
+        .attr("class", "tile")
+        .attr("width", 0.95 * TILE_SIZE)
+        .attr("height", 0.95 * TILE_SIZE)
+        .attr("rx", 3)
+        .attr("ry", 3)
+        .attr("x", function (d) { return i2x(d.i); })
+        .attr("y", function (d) { return j2y(d.j); });
 
 }
 
 
+// only single photon sources
 Board.prototype.stateInit = function () {
 
   var sources = 0;
@@ -54,6 +97,26 @@ Board.prototype.stateInit = function () {
     console.log("As of now only one source, sorry.");
   }
 
+};
+
+
+// as of now - only single particle interactions
+Board.prototype.statePropagate = function () {
+
+  var stateSpatial0 = this.stateSpatial;
+  var stateSpatial = [];
+
+  // _.chain(stateSpatial0)
+  //   .groupBy(function (each) { return [each.i, each.j].join(" "); })
+  //   .map(function ())
+
+
+
+  // _.groupBy(this.stateSpatial, function (vIn) {
+  //   vIn 
+  // })
+
 }
+
 
 
---
+++ main.js
@@ -0,0 +1,6 @@
+
+var board = new Board(5, 5);
+
+board.draw();
+
+
