c0d2ecc03373b771cffb4a7d82699c959bacae24
Patryk Hes <patryk.hes@laboratorium.ee>
2015-05-19 13:48:30 +0200

    feat(board): Rotation and drag'n'drop.

--- js/board.js
+++ js/board.js
@@ -10,17 +10,23 @@ export class Board {
   constructor(level, svg) {
     this.level = level;
     this.svg = svg;
-    this.tiles = [];
+    this.tileMatrix = [];
+    this.tileList = [];
+    this.tileSelection = d3.select();
   }
+
   clear() {
     // Create matrix filled with Vacuum
-    this.tiles = _.range(this.level.width).map((i) =>
+    this.tileMatrix = _.range(this.level.width).map((i) =>
         _.range(this.level.height).map((j) =>
             new tile.Tile(tile.Vacuum, 0, false, i, j)
         )
     );
+    // Clear tile list
+    this.tileList = [];
     return this;
   }
+
   reset() {
     // Clear tiles
     this.clear();
@@ -29,7 +35,7 @@ export class Board {
       if (!_.has(tile, tileRecipe.name)) {
         return;
       }
-      this.tiles[tileRecipe.i][tileRecipe.j] = new tile.Tile(
+      this.tileMatrix[tileRecipe.i][tileRecipe.j] = new tile.Tile(
         tile[tileRecipe.name],
         tileRecipe.rotation || 0,
         !!tileRecipe.frozen,
@@ -37,64 +43,184 @@ export class Board {
         tileRecipe.j
       );
     });
+    // Generate flat list
+    this.tileList = _.flatten(this.tileMatrix);
     // Initial drawing
     this.resizeSvg();
     this.drawBackground();
     this.drawBoard();
   }
+
   resizeSvg() {
     this.svg.attr({
       viewBox: `0 0 ${config.tileSize * this.level.width} ${config.tileSize * this.level.height}`
     });
   }
+
   drawBackground() {
     this.svg
       .append('g')
       .attr('class', 'background')
       .selectAll('.tile')
-      .data(_.flatten(this.tiles))
+      .data(this.tileList)
       .enter()
-        .append('rect')
-        .attr({
-          'class': (d) => {
-            if (d.frozen) {
-              return 'tile tile--frozen';
-            } else {
-              return 'tile';
-            }
-          },
-          x: (d) => d.x,
-          y: (d) => d.y,
-          width: config.tileSize,
-          height: config.tileSize,
-        });
+      .append('rect')
+      .attr({
+        'class': (d) => {
+          if (d.frozen) {
+            return 'tile tile--frozen';
+          } else {
+            return 'tile';
+          }
+        },
+        x: (d) => d.x,
+        y: (d) => d.y,
+        width: config.tileSize,
+        height: config.tileSize,
+      });
   }
+
   drawBoard() {
-    const tiles = this.svg
+    this.spawnTiles();
+    this.keepNodeReference();
+    this.drawTiles();
+    this.drawHitboxes();
+    this.bindClick();
+    this.bindDrag();
+  }
+
+  spawnTiles() {
+    this.tileSelection = this.svg
       .append('g')
       .attr('class', 'board')
       .selectAll('.tile')
       .data(_.filter(
-        _.flatten(this.tiles),
+        this.tileList,
         (t) => t.type !== tile.Vacuum
       ))
       .enter()
-        .append('g')
-        .attr({
-          'class': 'tile',
-          transform: (d) => `translate(${d.x + config.tileSize/2},${d.y +  config.tileSize/2})`
-        });
+      .append('g')
+      .attr({
+        'class': 'tile',
+        transform: (d) => `translate(${d.x + config.tileSize / 2},${d.y + config.tileSize / 2})`
+      });
+  }
 
-    // Keep node reference
-    tiles.each((d) => d.node = this);
+  keepNodeReference() {
+    this.tileSelection
+      .each(function (d) {
+        d.node = this;
+      });
+  }
 
-    // Create image
-    tiles
+  drawTiles() {
+    this.tileSelection
       .append('use')
       .attr({
         'xlink:href': (d) => `#${d.type.name}`,
         'class': 'element',
         transform: (d) => `rotate(${d.type.rotationAngle * d.rotation},0,0)`
       });
+  }
+
+  drawHitboxes() {
+    this.tileSelection
+      .append('use')
+      .attr({
+        'xlink:href': '#hitbox',
+        'class': 'hitbox',
+      });
+  }
+
+  bindClick() {
+    this.tileSelection
+      .select('.hitbox')
+      .on('click', function (d) {
+        // Avoid rotation when dragged
+        if (d3.event.defaultPrevented) {
+          return;
+        }
+        const element = d3.select(d.node).select('.element');
+        d.rotation = (d.rotation + 1) % d.type.maxRotation;
+        // Assure that rotation animation is clockwise
+        const startAngle = d.type.rotationAngle * (d.rotation - 1);
+        element
+          .attr('transform', `rotate(${startAngle},0,0)`);
+        // Rotation animation
+        const endAngle = d.type.rotationAngle * d.rotation;
+        element
+          .transition()
+          .duration(config.rotationSpeed)
+          .attr('transform', `rotate(${endAngle},0,0)`);
+      });
+  }
+
+  bindDrag() {
+    function reposition(data, elem) {
+      delete data.newI;
+      delete data.newJ;
+      elem
+        .transition()
+        .duration(config.repositionSpeed)
+        .attr(
+          'transform',
+          `translate(${data.x + config.tileSize / 2},${data.y + config.tileSize / 2})`
+        );
+    }
+
+    const drag = d3.behavior.drag();
+    drag
+      .on('dragstart', () => {
+        d3.event.sourceEvent.stopPropagation();
+      })
+      .on('drag', function (d) {
+        d3.select(this)
+          .attr('transform', `translate(${d3.event.x},${d3.event.y})`);
+        d.newI = Math.floor(d3.event.x / config.tileSize);
+        d.newJ = Math.floor(d3.event.y / config.tileSize);
+      })
+      .on('dragend', (source) => {
+        // No drag? Return.
+        if (source.newI == null || source.newJ == null) {
+          return;
+        }
+
+        // Find source element
+        const sourceElem = d3.select(source.node);
+
+        // Drag ended outside of board? Reposition source and return.
+        if (
+             source.newI < 0 || source.newI >= this.level.width
+          || source.newJ < 0 || source.newJ >= this.level.height
+        ) {
+          reposition(source, sourceElem);
+          return;
+        }
+
+        // Find target and target element
+        const target = this.tileMatrix[source.newI][source.newJ];
+        const targetElem = d3.select(target.node || null);
+
+        // Is it impossible to swap items? Reposition source and return.
+        if (source.frozen || target.frozen) {
+          reposition(source, sourceElem);
+          return;
+        }
+
+        // Swap items in matrix
+        [this.tileMatrix[source.i][source.j], this.tileMatrix[target.i][target.j]] =
+        [this.tileMatrix[target.i][target.j], this.tileMatrix[source.i][source.j]];
+
+        // Swap items positions
+        [source.i, source.j, target.i, target.j] =
+        [target.i, target.j, source.i, source.j];
+
+        // Reposition both elements
+        reposition(source, sourceElem);
+        reposition(target, targetElem);
+      });
+
+    this.tileSelection
+      .call(drag);
   }
 }
--- js/config.js
+++ js/config.js
@@ -1,2 +1,8 @@
 // Tile size in px
 export const tileSize = 100;
+// Rotation speed in ms
+export const rotationSpeed = 250;
+// Tile reposition speed in ms
+export const repositionSpeed = 250;
+
+
