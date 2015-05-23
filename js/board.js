b86e18d3bfcc6f20b82c546afca0b04d8158cf8b
Patryk Hes <patryk.hes@laboratorium.ee>
2015-05-23 12:53:49 +0200

    fix(board): fixed previous commit

--- js/board.js
+++ js/board.js
@@ -197,7 +197,7 @@ export class Board {
         d3.event.sourceEvent.stopPropagation();
 
         // Move element to the top
-        this.svg.select('.tiles')[0][0].appendChild(source.node);
+        this.svg.select('.board')[0][0].appendChild(source.node);
       })
       .on('drag', function (source) {
         // Is it impossible to drag item?
