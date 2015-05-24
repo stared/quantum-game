08e35946e6ff61562da74bc2ae24d4c371f122e2
Patryk Hes <patryk.hes@laboratorium.ee>
2015-05-24 15:22:38 +0200

    feat(particleAnimation): SVG and canvas animation both in codebase

--- js/particles.js
+++ js/particles.js
@@ -1,8 +1,9 @@
 /*global window:false*/
 import _ from 'lodash';
+import d3 from 'd3';
 
 import {TAU, velocityI, velocityJ, perpendicularI, perpendicularJ} from './const';
-import {animationStepDuration, tileSize, oscillations, polarizationScaleH, polarizationScaleV} from './config';
+import {animationStepDuration, tileSize, oscillations, polarizationScaleH, polarizationScaleV, resizeThrottle} from './config';
 
 class Particle {
 
@@ -33,17 +34,16 @@ class Particle {
   }
 
   get prob() {
-    return this.hRe*this.hRe + this.hIm*this.hIm + this.vRe*this.vRe + this.vIm*this.vIm;
+    return this.hRe * this.hRe + this.hIm * this.hIm
+         + this.vRe * this.vRe + this.vIm * this.vIm;
   }
 
 }
 
-export class ParticleAnimation {
-
+class ParticleAnimation {
   constructor(history, board) {
 
     this.history = history.map((state) => {
-      // console.log("groupBy", _.groupBy(state, (val) => `${val.i},${val.j},${val.to[0]}`))
       return _.chain(state)
         .groupBy((val) => `${val.i},${val.j},${val.to[0]}`)
         .mapValues((ray) => {
@@ -62,20 +62,25 @@ export class ParticleAnimation {
 
     this.board = board;
     this.stepNo = 0;
-    this.particleGroup = this.board.svg
-      .select('.particles');
+  }
+}
 
+export class SVGParticleAnimation extends ParticleAnimation {
+  constructor(history, board) {
+    super(history, board);
+    this.particleGroup = null;
     this.currentTimeout = 0;
   }
 
   stop() {
     window.clearTimeout(this.currentTimeout);
-    this.particleGroup
-      .selectAll('.particle')
-      .remove();
+    this.particleGroup.remove();
   }
 
   play() {
+    this.particleGroup = this.board.svg
+      .append('g')
+      .attr('class', 'board');
     this.nextFrame();
   }
 
@@ -92,7 +97,6 @@ export class ParticleAnimation {
   }
 
   updateParticles() {
-
     const particles = this.particleGroup
       .selectAll('.particle')
       .data(this.history[this.stepNo]);
@@ -128,7 +132,100 @@ export class ParticleAnimation {
         const y = (1 - t) * d.startY + t * d.endY + perpendicularJ[d.dir] * h;
         const s = 1 + polarizationScaleV * (d.vRe * Math.cos(oscillations * TAU * t) + d.vIm * Math.sin(oscillations * TAU * t)) / Math.sqrt(d.prob);
         return `translate(${x}, ${y}) scale(${s})`;
-      })
+      });
+  };
+}
 
+export class CanvasParticleAnimation extends ParticleAnimation {
+  constructor(history, board) {
+    super(history, board);
+    this.canvas = null
+    this.ctx = null;
+    this.startTime = 0;
+    // Prepare throttled version of resizeCanvas
+    this.throttledResizeCanvas =
+      _.throttle(this.resizeCanvas, resizeThrottle)
+      .bind(this);
+  }
+
+  stop() {
+    window.removeEventListener('resize', this.throttledResizeCanvas);
+    if (this.canvas) {
+      this.canvas.remove();
+    }
+  }
+
+  play() {
+    // Create canvas, get context
+    this.canvas = d3.select('#game')
+      .append('canvas');
+    this.ctx = this.canvas[0][0].getContext('2d');
+    // Stop animation when clicked on canvas
+    this.canvas[0][0].addEventListener('click', this.stop.bind(this));
+    // Initial canvas resize
+    this.resizeCanvas();
+    // Set resize event handler
+    window.addEventListener('resize', this.throttledResizeCanvas);
+    this.startTime = new Date().getTime();
+    window.requestAnimationFrame(this.nextFrame.bind(this));
+  }
+
+  resizeCanvas() {
+    // Get the size of #game > svg > .background element
+    const box = this.board.svg.select('.background').node().getBoundingClientRect();
+    this.canvas
+      .style({
+        width:  `${Math.round(box.width)}px`,
+        height: `${Math.round(box.height)}px`,
+        top:    `${Math.round(box.top)}px`,
+        left:   `${Math.round(box.left)}px`,
+      })
+      .attr({
+        width:  this.board.level.width * tileSize,
+        height: this.board.level.height * tileSize,
+      });
+  }
+
+  nextFrame() {
+    const time = new Date().getTime();
+    const stepFloat = (time - this.startTime) / animationStepDuration;
+    this.stepNo = Math.floor(stepFloat);
+
+    if (this.stepNo < this.history.length) {
+      this.updateParticles(stepFloat - this.stepNo);
+      window.requestAnimationFrame(this.nextFrame.bind(this));
+    } else {
+      this.stop();
+    }
+  }
+
+  /**
+   * Draw particles basing on current step (this.stepNo)
+   * and t, which represents how far we are in progression
+   * from step stepNo to step (stepNo + 1).
+   * t is in range [0, 1).
+   */
+  updateParticles(t) {
+    this.ctx.clearRect(
+      0, 0,
+      this.board.level.width * tileSize,
+      this.board.level.height * tileSize
+    );
+    this.ctx.fillStyle = 'coral';
+    _.each(this.history[this.stepNo], (d) => {
+      this.ctx.beginPath();
+      this.ctx.globalAlpha = d.prob;
+      const h = polarizationScaleH * (d.hRe * Math.cos(oscillations * TAU * t) + d.hIm * Math.sin(oscillations * TAU * t)) / Math.sqrt(d.prob);
+      const x = (1 - t) * d.startX + t * d.endX + perpendicularI[d.dir] * h;
+      const y = (1 - t) * d.startY + t * d.endY + perpendicularJ[d.dir] * h;
+      const s = 10 * (
+          1 + polarizationScaleV * (
+              d.vRe * Math.cos(oscillations * TAU * t)
+            + d.vIm * Math.sin(oscillations * TAU * t)
+          ) / Math.sqrt(d.prob)
+        );
+      this.ctx.arc(x, y, s, 0, 360, false);
+      this.ctx.fill();
+    });
   }
 }
