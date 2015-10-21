6881f9c3bb982e3be7941e9a0be6df1689c3689b
Piotr Migdal <pmigdal@gmail.com>
2015-10-21 18:25:56 +0200

    chore(config) units in brackets

--- js/config.js
+++ js/config.js
@@ -1,12 +1,12 @@
-// Tile size in px
+// Tile size (px)
 export const tileSize = 100;
-// Rotation speed in ms
+// Rotation speed (ms)
 export const rotationSpeed = 250;
-// Tile reposition speed in ms
+// Tile reposition speed (ms)
 export const repositionSpeed = 250;
 // Maximum iteration count
 export const maxIterations = 1000;
-// Animation step duration in ms
+// Animation step duration (ms)
 export const animationStepDuration = 600;
 // Oscillations per tile
 export const oscillations = 1;
@@ -14,7 +14,7 @@ export const oscillations = 1;
 export const polarizationScaleH = 15;
 // Vertical oscillation scale (factor)
 export const polarizationScaleV = 0.7;
-// Canvas resize throttling in ms
+// Canvas resize throttling (ms)
 export const resizeThrottle = 100;
-// Absorption animation duration in ms
+// Absorption animation duration (ms)
 export const absorptionDuration = 2000;
