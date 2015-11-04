060453f9bf5ea4eda2d426749e4997487d4645e9
Patryk Hes <patryk.hes@laboratorium.ee>
2015-11-04 01:44:47 +0100

    feat(stock): drawing and reloading stock with level change

--- js/stock.js
+++ js/stock.js
@@ -5,37 +5,55 @@ import * as tile from './tile';
 
 export class Stock {
   constructor(level) {
-    this.stock = this.initializeStock(level);
-    this.maxStock = this.initializeMaxStock(this.stock, level);
+    this.stock = {};
+    this.initialCount(level);
+    this.maxCount(level);
   }
 
   // Calculate how many and which tiles there are in the beginning in the stock.
-  initializeStock(level) {
-    const stock = {};
+  initialCount(level) {
     // Initialize empty stock - map from all non-vacuum tiles to their count: 0
     _.forEach(tile.nonVacuumTiles, (tileName) => {
-      stock[changeCase.pascalCase(tileName)] = 0;
+      this.stock[changeCase.pascalCase(tileName)] = {
+        currentCount: 0,
+      };
     });
     // Go through the level's initial stock
     if (level.initialStock) {
       _.forEach(level.initialStock, (tileCount, tileName) => {
-        stock[changeCase.pascalCase(tileName)] = tileCount;
+        this.stock[changeCase.pascalCase(tileName)].currentCount = tileCount;
       });
     }
-    return stock;
   }
 
   // Calculate how much tiles there can possibly be in the stock.
-  initializeMaxStock(stock, level) {
-    const maxStock = _.clone(stock);
+  maxCount(level) {
+    // Copy stock information to maxStock
+    _.forEach(tile.nonVacuumTiles, (tileName) => {
+      this.stock[changeCase.pascalCase(tileName)].maxCount =
+        this.stock[changeCase.pascalCase(tileName)].currentCount;
+    });
     // Go through the level's initial tiles
     if (level.tileRecipes) {
       _.forEach(level.tileRecipes, (tileDef) => {
         if (!tileDef.frozen) {
-          maxStock[changeCase.pascalCase(tileDef.name)]++;
+          this.stock[changeCase.pascalCase(tileDef.name)].maxCount++;
         }
       });
     }
-    return maxStock;
+  }
+
+  // Return actually useful stock cells, e.g. filter out those that
+  // have no chance of being used.
+  // Use the ordering of stock items as in tile.nonVacuumTiles.
+  usedStockNames() {
+    const usedStockNames = [];
+    _.forEach(tile.nonVacuumTiles, (tileName) => {
+      tileName = changeCase.pascalCase(tileName);
+      if (this.stock[tileName].maxCount > 0) {
+        usedStockNames.push(tileName);
+      }
+    });
+    return usedStockNames;
   }
 }
