9c869f514dfb3543992b025d6a90ce8e420b5d87
Piotr Migdal <pmigdal@gmail.com>
2015-12-30 13:39:38 +0100

    feat(test) random vectors for testing unitarity on subspace

--- js/tensor/full.spec.js
+++ js/tensor/full.spec.js
@@ -6,27 +6,52 @@ function probability(entry) {
   return entry.re * entry.re + entry.im * entry.im;
 }
 
+const subspaceAll = ['>-', '>|', '^-', '^|', '<-', '<|', 'v-', 'v|'];
+const subspaceDirHorizontal = ['>-', '>|', '<-', '<|'];
+const subspaceDirVertical = ['^-', '^|', 'v-', 'v|'];
+
+// calculates norm of a random unit vector within a subspace
+function matrixNormOnRandomVector(matrix, subspace = subspaceAll) {
+  const inputVector = subspace.map((key) => [key, {re: Math.random(), im: Math.random()}]);
+  const norm = _.sum(inputVector, (input) => probability(input[1]));
+  const outputVector = {};
+  let zIn;
+  inputVector.forEach((input) => {
+    zIn = input[1];
+    matrix.get(input[0]).forEach((zOut, keyOut) => {
+      if (!_.has(outputVector, keyOut)) {
+        outputVector[keyOut] = {re: 0, im: 0};
+      }
+      outputVector[keyOut].re += zIn.re * zOut.re - zIn.im * zOut.im;
+      outputVector[keyOut].im += zIn.re * zOut.im + zIn.im * zOut.re;
+    });
+  });
+
+  return _.sum(outputVector, probability) / norm;
+}
+
+
 describe('thinSplitter', () => {
+
   it('should consist of 4 tensors', () => {
     expect(full.thinSplitter.length).toBe(4);
   });
 
   it('diagonal orientations should consist of unitary tensors', () => {
-    _.each(_.at(full.thinSplitter, [1, 3]), (tensor) => {
-      for (let entry of tensor.map.values()) {
-        const probabilities = _.sum(_.map([...entry.values()], probability));
-        expect(probabilities).toBeCloseTo(1, 5);
-      }
-    });
+    expect(matrixNormOnRandomVector(full.thinSplitter[1].map)).toBeCloseTo(1, 5);
+    expect(matrixNormOnRandomVector(full.thinSplitter[3].map)).toBeCloseTo(1, 5);
   });
 
-  it('should pass half of the beam perpendicular to the element', () => {
-    // 2nd element represents splitter's "|" rotation,
-    // so it's perpendicular to ">" beam direction.
-    const transition = full.thinSplitter[2].map.get('>-');
+  it('| and - orientations should be unitary for perpendicular directions', () => {
 
-    expect(transition.size).toBe(2);
-    expect(transition.get('>-')).toBeDefined();
-    expect(transition.get('<-')).toBeDefined();
+    expect(matrixNormOnRandomVector(
+      full.thinSplitter[0].map, subspaceDirVertical
+    )).toBeCloseTo(1, 5);
+
+    expect(matrixNormOnRandomVector(
+      full.thinSplitter[2].map, subspaceDirHorizontal
+    )).toBeCloseTo(1, 5);
+
   });
+
 });
