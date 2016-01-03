617265d560405b0d94ab58207eeb0c4307ce3b53
Piotr Migdal <pmigdal@gmail.com>
2016-01-04 00:10:44 +0100

    feat(winningStatus) required no detection prob at mines

--- js/winning_status.js
+++ js/winning_status.js
@@ -34,25 +34,31 @@ export class WinningStatus {
       .pluck('probability')
       .value();
 
-    // maybe also 'probability at mine'?
-
     this.totalProbAtDets = _.sum(this.probsAtDets);
     this.noOfFedDets = this.probsAtDets
       .filter((probability) => probability > EPSILON_DETECTION)
       .length;
+    this.probsAtMines = _(this.absorptionProbabilities)
+      .filter((entry) =>
+        this.tileMatrix[entry.i] && this.tileMatrix[entry.i][entry.j] && this.tileMatrix[entry.i][entry.j].tileName === 'Mine'
+      )
+      .sum('probability');
   }
 
   compareToObjectives(requiredDetectionProbability, detectorsToFeed) {
     this.enoughProbability = this.totalProbAtDets > requiredDetectionProbability - EPSILON_DETECTION;
     this.enoughDetectors = this.noOfFedDets >= detectorsToFeed;
-    this.isWon = this.enoughProbability && this.enoughDetectors;
+    this.noExplosion = this.probsAtMines < EPSILON_DETECTION;
+    this.isWon = this.enoughProbability && this.enoughDetectors && this.noExplosion;
 
     if (this.isWon) {
       this.message = 'You did it!';
+    } else if (!this.noExplosion) {
+      this.message = `Nothing else matters when you have ${(100 * this.probsAtMines).toFixed(0)}% chance of setting off a mine!`;
     } else if (this.enoughProbability) {
       this.message = `${detectorsToFeed - this.noOfFedDets} detector feels sad and forgotten. Be fair! Give some chance to every detector!`;
     } else if (this.totalProbAtDets > EPSILON_DETECTION) {
-      this.message = `Only ${(100 * this.totalProbAtDets).toFixed(0)}% (out of ${(100 * requiredDetectionProbability).toFixed(0)}%) chance of detecting a photon at a detector. Try harder!`
+      this.message = `Only ${(100 * this.totalProbAtDets).toFixed(0)}% (out of ${(100 * requiredDetectionProbability).toFixed(0)}%) chance of detecting a photon at a detector. Try harder!`;
     } else {
       this.message = 'No chance to detect a photon at a detector.';
     }
