1cd8ba264a0bc2863f410778147f312cee29594a
Piotr Migdal <pmigdal@gmail.com>
2016-03-26 19:17:47 +0100

    fix(deploy) works for new jspm

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -1,5 +1,5 @@
 # the only other step is updating index.html file!
-jspm bundle-sfx -minify app
+jspm bundle-sfx --minify app.js
 rm bundled/build.js*
 mv build.js* bundled/
 s3cmd sync bundled/index.html s3://quantumgame.io/dev/
