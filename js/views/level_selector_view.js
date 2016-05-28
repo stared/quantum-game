a6fbd46c84b767227feef5c0e358975d955939df
Piotr Migdal <pmigdal@gmail.com>
2016-05-28 17:33:52 +0200

    feat(levelSelectorView) elements used in a level - for dev

--- js/views/level_selector_view.js
+++ js/views/level_selector_view.js
@@ -1,4 +1,5 @@
 import d3 from 'd3';
+import _ from 'lodash';
 
 import {View} from './view';
 import * as level from '../level';
@@ -14,16 +15,45 @@ export class LevelSelectorView extends View {
     return 'view--level-selector';
   }
   initialize() {
-    d3.select('.level-selector > ul')
+    const listOfElements = d3.select('.level-selector > ul')
       .selectAll('li')
       .data(level.levels)
       .enter()
       .append('li')
       .attr('class', 'level-item unselectable')
-      .text((d) => `[${d.group}] ${d.i}. ${d.name}`)
+      .text((d) => `[${d.group}] ${d.i}. ${d.name} `)
       .on('click', (d) => {
         this.game.gameBoard.loadLevel(d.id);
         this.game.setView('game');
       });
+
+    // as of now it is a version for developers
+    // for users - graphical icons (of the new elements) or display:none;
+    const elementsEncountered = {};
+    level.levels.forEach((d) => {
+      d.newTiles = [];
+      d.tiles.forEach((tile) => {
+        if (!_.has(elementsEncountered, tile.name)) {
+          elementsEncountered[tile.name] = true;
+          d.newTiles.push(tile.name);
+        }
+      });
+    });
+
+    listOfElements.append('span')
+      .style('font-size', '1.5vh')
+      .text((d) =>
+        _(d.tiles)
+          .groupBy('name')
+          .keys()
+          .filter((tile) => !_.includes(['Detector', 'Rock', 'Source'], tile))
+          .value()
+          .join(' ')
+      );
+
+    listOfElements.append('span')
+      .style('font-size', '1.5vh')
+      .text((d) => d.newTiles.length ? ` (NEW: ${d.newTiles.join(' ')})` : '');
+
   }
 }
