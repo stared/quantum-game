a1f211b69d109490ea8e7949bf4e259bd337a02c
Piotr Migdal <pmigdal@gmail.com>
2016-06-01 14:53:44 +0200

    chore(deployment) production and images

---
+++ deploy_play.sh
@@ -0,0 +1,11 @@
+# the only other step is updating index.html file!
+jspm bundle-sfx --minify app.js
+rm bundled/build.js*
+mv build.js* bundled/
+s3cmd sync bundled/index.html s3://play.quantumgame.io/
+s3cmd sync bundled/build.js s3://play.quantumgame.io/
+s3cmd sync --recursive css s3://play.quantumgame.io/
+s3cmd sync --recursive sounds s3://play.quantumgame.io/
+s3cmd sync bg.jpg s3://play.quantumgame.io/
+s3cmd sync logo.svg s3://play.quantumgame.io/
+s3cmd sync favicon.ico s3://play.quantumgame.io/
