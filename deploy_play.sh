60b9be69ee261d7926bcf65473ed21cd679271c7
Piotr Migdal <pmigdal@gmail.com>
2016-06-03 19:41:44 +0200

    chore(deployment) fb ograph screenshot

--- deploy_play.sh
+++ deploy_play.sh
@@ -9,3 +9,4 @@ s3cmd sync --recursive sounds s3://play.quantumgame.io/
 s3cmd sync bg.jpg s3://play.quantumgame.io/
 s3cmd sync logo.svg s3://play.quantumgame.io/
 s3cmd sync favicon.ico s3://play.quantumgame.io/
+s3cmd sync screenshot_qg_dev.png s3://play.quantumgame.io/
